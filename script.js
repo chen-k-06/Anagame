25a1dba434cd4b1f0f02424850a700235a19f01a
Kexin <kkc2160@columbia.edu>
2025-07-09 15:51:21 +0100

    Added input, right container contents

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ let timeLeft = 60 * 100; // 60 seconds -> 60 * 1000 milliseconds
 const timer = document.getElementById('timer');
 
 function updateTimer() {
+    // timeLeft = 0
     const seconds = Math.floor(timeLeft / 100);
     const ms = Math.floor((timeLeft % 100));
     timer.textContent = `${seconds.toString().padStart(2, '0')}:${ms.toString().padStart(2, '0')}`;
     if (timeLeft <= 0) {
         clearInterval(timerId);
-        timer.textContent = "Time's up!";
+        timer.textContent = "Done!";
         play_button.classList.remove("disabled")
         in_game = false;
+        document.getElementById('slider').disabled = false;
+        document.getElementById('play-button').disabled = false;
         console.log("Game over.")
     }
 
@@ -27,6 +30,11 @@ play_button.addEventListener("click", () => {
         console.log("Already in a game.");
         return;
     }
+
+    // disable play button and slider
+    document.getElementById('slider').disabled = true;
+    document.getElementById('play-button').disabled = true;
+
     in_game = true;
     console.log("Game started.")
     play_button.classList.add("disabled")
