8ef52a9555a92986dc7e950dcbacae25600b32c0
Kexin <kkc2160@columbia.edu>
2025-07-09 04:53:19 +0100

    Finalized timer logic

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 /*
 Timer function logic
 */
-let timeLeft = 6000; // seconds
+let timeLeft = 60 * 100; // 60 seconds -> 60 * 1000 milliseconds
 const timer = document.getElementById('timer');
 
 function updateTimer() {
-    const seconds = Math.floor(timeLeft / 60);
-    const ms = timeLeft % 100;
-    timer.textContent = `${seconds.toString().padStart(2, '0')}:${ms.toString().padStart(3, '0')}`;
+    const seconds = Math.floor(timeLeft / 100);
+    const ms = Math.floor((timeLeft % 100));
+    timer.textContent = `${seconds.toString().padStart(2, '0')}:${ms.toString().padStart(2, '0')}`;
     if (timeLeft <= 0) {
         clearInterval(timerId);
         timer.textContent = "Time's up!";
     }
 
-    timeLeft--;
+    timeLeft -= 1;
 }
 
 updateTimer();
-const timerId = setInterval(updateTimer, 1);
+const timerId = setInterval(updateTimer, 10); // call updateTimer every millisecond
