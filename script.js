b44eba99b1469b736a4dd1f205c79463b6bd6d10
Kexin <kkc2160@columbia.edu>
2025-07-12 07:42:08 +0100

    API debugging x2

--- script.js
+++ script.js
@@ -80,11 +80,12 @@ async function getLetters(distribution, fun_factor) {
         return;
     }
 
+    console.log("Sending:", JSON.stringify({
+        fun_factor: fun_factor,
+        distribution: distribution
+    }));
+
     try {
-        console.log("Sending:", JSON.stringify({
-            fun_factor: fun_factor,
-            distribution: distribution
-        }));
 
         const response = await fetch('https://anagame.onrender.com/get_letters', {
             method: 'POST',
