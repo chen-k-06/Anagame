4cc245781813a35e7bdc8037f32fbd3ee998c64c
Kexin <kkc2160@columbia.edu>
2025-07-23 21:18:47 +0100

    Added stats update

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-# api calls
-# actual endpoint is https://api-hosting-cdnc.onrender.com
-from AnagramExplorer import AnagramExplorer
-from valid_anagame_words import get_valid_word_list
-from anagame import calc_stats, generate_letters
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
@@ -10,8 +5,8 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["https://chen-k-06.github.io"],  # frontend URL
+    CORSMiddleware, 
+    allow_origins = ["https://chen-k-06.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -22,13 +17,24 @@ def read_root():
     return {"Hello": "World"}
 
 #------------------------------------------------
+#------------------------------------------------
+# ANAGAME 
+#------------------------------------------------
+#------------------------------------------------
+
+from AnagramExplorer import AnagramExplorer
+from valid_anagame_words import get_valid_word_list
+from anagame import calc_stats, generate_letters
+from typing import List, Tuple
+
+#------------------------------------------------
 # Calculate end of game statistics functions
 #------------------------------------------------
 class GetLetters(BaseModel):
     fun_factor: int
     distribution: str
     
-@app.post("/get_letters")
+@app.post("/anagame_get_letters")
 def handle_get_letters(request: GetLetters) -> list[str]: 
     explorer = AnagramExplorer(get_valid_word_list())
     result = generate_letters(request.fun_factor, request.distribution, explorer)
@@ -38,15 +44,41 @@ def handle_get_letters(request: GetLetters) -> list[str]:
 # Calculate end of game statistics functions 
 #------------------------------------------------
 class CalcStats(BaseModel):
-    guesses: list[list[str]]
-    letters: list[str]
+    guesses: List[Tuple[str, str]]
+    letters: List[str]
 
-@app.post("/calc_stats")
-def handle_calc_stats(request: CalcStats) -> list[str]: 
+class StatsResponse(BaseModel):
+    valid_guesses: List[List[str]]
+    invalid_guesses: List[List[str]]
+    score: int
+    accuracy: float
+    skill: float
+    guessed_words: List[str]
+    not_guessed_words: List[str]
+
+@app.post("/anagame_calc_stats", response_model=StatsResponse)
+def handle_calc_stats(request: CalcStats) -> StatsResponse: 
     explorer = AnagramExplorer(get_valid_word_list())
     result = calc_stats(request.guesses, request.letters, explorer) #guesses needs to be tuples
-    return result
+        
+    return StatsResponse(
+        valid_guesses=result[0],
+        invalid_guesses=result[1],
+        score=result[2],
+        accuracy=result[3],
+        skill=result[4],
+        guessed_words=result[5],
+        not_guessed_words=result[6]
+    )
 
 #------------------------------------------------
 # Get all anagrams (for hint)
 #------------------------------------------------
+class GetHint(BaseModel):
+    letters: list[str]
+    
+@app.post("/anagame_get_hint")
+def handle_get_letters(request: GetHint) -> str: 
+    explorer = AnagramExplorer(get_valid_word_list())
+    result = explorer.get_most_anagrams(request.letters)
+    return result
